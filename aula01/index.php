185e4e6ff78f3228ca4aee4f4379a0d6fca33981
Amanda Martins <72510249+martins-amanda@users.noreply.github.com>
2022-04-11 00:25:43 -0300

    Update index.php

--- aula01/index.php
+++ aula01/index.php
@@ -5,7 +5,7 @@
     body{
         background: rgb(169, 239, 277)
     }
-   H1 {
+   h1 {
        text-align: center;
        font-size: 24px;
        color:black ;
@@ -27,19 +27,19 @@
 
 <body>
 
-    <H1>AULA 01</H1>
+    <h1>AULA 01</h1>
     <p>A seguir, temos a lista de scripts trabalhadosna aula 01</p>
-    <H2><a href='script_01.php'>Script 01</a></H2>
-    <H2><a href='script_02.php'>Script 02</a></H2>
-    <H2><a href='script_03.php'>Script 03</a></H2>
-    <H2><a href='script_04.php'>Script 04</a></H2>
-    <H2><a href='script_05.php'>Script 05</a></H2>
-    <H2><a href='script_06.php'>Script 06</a></H2>
-    <H2><a href='script_07.php'>Script 07</a></H2>
+    <h2><a href='script_01.php'>Script 01</a></h2>
+    <h2><a href='script_02.php'>Script 02</a></h2>
+    <h2><a href='script_03.php'>Script 03</a></h2>
+    <h2><a href='script_04.php'>Script 04</a></h2>
+    <h2><a href='script_05.php'>Script 05</a></h2>
+    <h2><a href='script_06.php'>Script 06</a></h2>
+    <h2><a href='script_07.php'>Script 07</a></h2>
      
-    <H1>Links Úteis:</H1>
-    <H2><a href='https://www.w3schools.com/php/phptryit.asp?filename=tryphp_compiler'>Editor Online</a></H2>
-    <!-- <H2><a href='atividade/index.php'>Atividade 1</a></H2> -->
+    <h1>Links Úteis:</h1>
+    <h2><a href='https://www.w3schools.com/php/phptryit.asp?filename=tryphp_compiler'>Editor Online</a></h2>
+    <!-- <h2><a href='atividade/index.php'>Atividade 1</a></h2> -->
 </body>
 
 </html>
